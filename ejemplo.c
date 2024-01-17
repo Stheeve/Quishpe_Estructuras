02193d9d3fd0bd827557c9950312d7e73bc872af
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2024-01-16 22:08:29 -0500

    Se inicializo un do para poder hacer el menu

--- ejemplo.c
+++ ejemplo.c
@@ -10,3 +10,22 @@ struct alumno
     float nota;
 };
 
+int main()
+{
+    int opcion;
+    struct alumno alumnos[20];
+    int estudiantes = 0;
+
+    do
+    {
+        printf("\nOpcion 1. Ingresar datos de un estudiante\n");
+        printf("Opcion 2. Mostrar todos los alumnos ingresados\n");
+        printf("Opcion 3. Salir\n");
+        printf("Coloque el numero de opcion\n");
+        scanf("%d", &opcion);
+
+        
+    } while (opcion != 3);
+
+    return 0;
+}
